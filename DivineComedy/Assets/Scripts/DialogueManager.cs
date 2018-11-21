ab65e0bea503121e3126431f601b5aa2a9da4288
Andrew Mathies <andrewmathies@yahoo.com>
2018-11-21 17:07:45 -0500

    starting on art

---
+++ DivineComedy/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour {
+
+    public TextMeshProUGUI nameText, dialogueText;
+
+    private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start () {
+        sentences = new Queue<string>();
+	}
+	
+	public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("started talking to " + dialogue.name);
+        nameText.SetText(dialogue.name);
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+    
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string curSentence = sentences.Dequeue();
+        Debug.Log(curSentence);
+        dialogueText.SetText(curSentence);
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("end of dialogue!");
+    }
+}
