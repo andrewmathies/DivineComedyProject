191481a3e1869b5dc8e965eb007aec4368d2a245
Andrew Mathies <andrewmathies@yahoo.com>
2018-11-21 14:40:35 -0500

    movement and camera done

---
+++ DivineComedy/Assets/Scripts/Controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    private float horizontalInput;
+    private Vector3 curPos, virgilPos;
+
+    public float minInput, moveDist, followDist;
+    public GameObject virgil;
+	
+	// Update is called once per frame
+	void Update () {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        curPos = transform.position;
+        virgilPos = virgil.transform.position;
+
+        if (horizontalInput > minInput)
+        {
+            // move right
+            transform.position = new Vector3(curPos.x + moveDist, curPos.y);
+            curPos.x = curPos.x + moveDist;
+        } else if (horizontalInput < minInput * -1)
+        {
+            // move left
+            transform.position = new Vector3(curPos.x - moveDist, curPos.y);
+            curPos.x = curPos.x - moveDist;
+        }
+
+        // make sure virgil is following dante!
+        if (virgilPos.x > curPos.x + followDist)
+        {
+            virgil.transform.SetPositionAndRotation(new Vector3(curPos.x + followDist, virgilPos.y), Quaternion.identity);
+        }
+        else if (virgilPos.x < curPos.x - followDist)
+        {
+            virgil.transform.SetPositionAndRotation(new Vector3(curPos.x - followDist, virgilPos.y), Quaternion.identity);
+        }
+    }
+}
+ 
